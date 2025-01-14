3ee6152009f257cd7f5d4eb0bdf96c60fef6e057
cameronharward <crh647@outlook.com>
2025-01-14 14:25:48 -0700

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber;
+        int answer;
+        string playAgain;
+        bool restart = true;
+        bool correct = false;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 100);
+            do{
+                Console.Write("Try to guess the magic number: ");
+                answer = int.Parse(Console.ReadLine());
+                if(answer < magicNumber){
+                    Console.WriteLine("Higher!");
+                }
+                else if(answer > magicNumber){
+                    Console.WriteLine("Lower!");
+                }
+                else{
+                    Console.WriteLine("Your nailed it!!!");
+                    correct = true;
+                }
+
+            }while(!correct);
+
+            correct = false;
+            Console.Write("Do you want to play again? yes or no: ");
+            playAgain = Console.ReadLine();
+            if(playAgain == "no"){
+                restart = false;
+                System.Console.WriteLine("Goodbye!");
+            }
+        } while(restart);
+       
     }
 }
